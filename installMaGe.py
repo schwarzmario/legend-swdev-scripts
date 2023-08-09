8e81d861623a0117f78ea4a4f6095d9991fcb781
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-08-09 13:03:46 +0200

    Add missing magepostproc path to PYTHONPATH

--- installMaGe.py
+++ installMaGe.py
@@ -127,7 +127,7 @@ with open('setup_mage.sh', 'w') as file:
                f'{install_path}/include/mage:' \
                f'{install_path}/include/mage-post-proc:' \
                '${ROOT_INCLUDE_PATH}\n')
-    file.write(f'export PYTHONPATH={install_path}/lib:$PYTHONPATH\n')
+    file.write(f'export PYTHONPATH={install_path}/lib:{install_path}/lib/magepostproc:$PYTHONPATH\n')
 
 # download (user will enter password)
 try:
